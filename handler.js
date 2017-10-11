8a6f9426f169b57e4ed4bc7fc6e0df9f6c402c65
Radovan Fekeč <radovan.fekec@sygictravel.com>
2017-10-11 22:24:32 +0200

    Implement basic api call chain

--- handler.js
+++ handler.js
@@ -1,15 +1,43 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!'
-    }),
-  };
+const axios = require('axios');
+const config = require('./config');
 
-  callback(null, response);
+module.exports.analyze = (event, context, callback) => {
+	const query = event.queryStringParameters.query;
+	const sourceLanguage = event.queryStringParameters.lang;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+	axios({
+		method: 'post',
+		url: 'https://translation.googleapis.com/language/translate/v2',
+		params: {
+			q: query,
+			source: sourceLanguage,
+			target: 'en',
+			format: 'text',
+			key: config.googleTranslateKey
+		}
+	}).then((responseGoogleTranslate) => {
+		const queryInTargetLang = responseGoogleTranslate.data.data.translations[0].translatedText
+		return axios({
+		method: 'get',
+		url: 'https://api.wit.ai/message',
+		headers: {'Authorization': config.witAiKey},
+		params: {
+			v: '11.10.2017',
+			q: queryInTargetLang
+		}
+	})
+	}).then((responseWitAi) => {
+
+		const response = {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: responseWitAi.data,
+			}),
+		};
+
+		callback(null, response);
+	});
+
 };
